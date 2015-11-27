3e00c1c5cdca72045e0ae38759d01c5c7e245b97
Bernardo Jose <b00063723@student.itb.ie>
2015-11-27 11:56:34 +0000

    added two buttons to save & load player position from the database

--- Assets/Scripts/Scene_Login/Login_Register.cs
+++ Assets/Scripts/Scene_Login/Login_Register.cs
@@ -8,42 +8,65 @@ using UnityEngine;
 using System.Collections;
 
 public class Login_Register : MonoBehaviour {
-
 	//veriables to store user and name 
-	public static string user = "", Uname = "";
+	    public string user = "", Uname = "";
 	//veriable to store user password (message veriable is used to print if a user is Successfull when loging in or unsuccessful
-	private string password = "", rePass = "", message = "";
+	public string password = "", rePass = "", message = "";
 	//boolen veriable to check if a user is registered 
 	private bool register = false;
+	private int id;
+	PlayerData player = new PlayerData();
+	SaveSystem syst = new SaveSystem();
+
+	public void setUser(string user){
+		this.user = user;
+	}
+
+	public string getUser(){
+		return user;
+	}
+	public void setUname(string Uname){
+		this.Uname = Uname;
+	}
 	
+	public string getUname(){
+		return Uname;
+	}
+	public void setPassword(string password){
+		this.password = password;
+	}
+	
+	public string getPassword(){
+		return password;
+	}
 	private void OnGUI()
 	{	//creates a GUI box to hold all the Gui components
-		GUI.Box (new Rect (340,1,700,490), "");
+		GUI.Box (new Rect (120,1,500,490), "");
 		//cretaes a A small Gui Box to display message
-		GUI.Box (new Rect (552,1,274,50), message);
+		GUI.Box (new Rect (116,1,504,50), message);
 
 		
 		if (register)//open register if statement
 		{
 			//Register Form GUI
-			GUI.Label(new Rect (552,90,270,20),"Username");
-			user = GUI.TextField(new Rect (552,110,280,20), user);
+			GUI.Label(new Rect (252,90,70,20),"Username");
+			user = GUI.TextField(new Rect (252,110,280,20), user);
 
-			GUI.Label(new Rect (552,138,280,20), "Name");
-			name = GUI.TextField (new Rect (552,156,280,20), name);
+			GUI.Label(new Rect (252,138,180,20), "Name");
+			Uname = GUI.TextField (new Rect (252,156,280,20), Uname);
 
-			GUI.Label (new Rect (552,190,280,20), "Password");
-			password = GUI.TextField(new Rect (552,210,280,20), password);
+			GUI.Label (new Rect (252,190,180,20), "Password");
+			password = GUI.TextField(new Rect (252,210,280,20), password);
 
-			GUI.Label(new Rect (552,240,280,20),"Re-password");
-			rePass = GUI.TextField(new Rect (552,260,280,20),rePass);
+			GUI.Label(new Rect (252,240,280,20),"Re-password");
+			rePass = GUI.TextField(new Rect (252,260,280,20),rePass);
 			
 
 			//nested if statements 
-			if (GUI.Button(new Rect (552,290,120,40),"Back"))
+			if (GUI.Button(new Rect (252,290,120,40),"Back"))
 				register = false;
 			
-			if (GUI.Button(new Rect (682,290,120,40),"Register"))
+			if (GUI.Button(new Rect (382,290,120,40),"Register"))
 			{
 				message = "";
 				
@@ -53,12 +76,19 @@ public class Login_Register : MonoBehaviour {
 				{
 					if (password == rePass)
 					{
-						WWWForm form = new WWWForm();
-						form.AddField("user", user);
-						form.AddField("name", name);
-						form.AddField("password", password);
-						WWW w = new WWW("http://localhost:8080/register.php", form);
-						StartCoroutine(registerFunc(w));
+
+						//setUname(Uname);
+						//setUser(user);
+						//setPassword();
+						Application.LoadLevel(2);
+						//player.setName(Uname);
+						//player.setUname(user);
+						//player.setPassword(password);
+						//saveScores();	
+						PlayerPrefs.SetString("user",user);
+						PlayerPrefs.SetString("uname",Uname);
+						PlayerPrefs.SetString("pass",password);
+
 					}
 					else
 						message += "Your Password does not match \n";
@@ -69,16 +99,16 @@ public class Login_Register : MonoBehaviour {
 		else
 		{ //login GUI
 			//this part of code is excuted first showing the login option
-			GUI.Label(new Rect (552,138,280,20),"User:");
-			user = GUI.TextField(new Rect (552,156,280,20),user);
-			GUI.Label(new Rect (552,190,280,20),"Password:");
-			password = GUI.TextField(new Rect (552,210,280,20),password); //,"*"[0]
+			GUI.Label(new Rect (252,138,280,20),"User:");
+			user = GUI.TextField(new Rect (252,156,280,20),user);
+			GUI.Label(new Rect (252,190,280,20),"Password:");
+			password = GUI.TextField(new Rect (252,210,280,20),password); //,"*"[0]
 
 			//if statement to check if a user has clicked the login button if true 
 			//the nested if statement will try check if user textfield and password textfield are empty
 			//else it will go and check the php login script to validate the user
 			//StartCorountin starts the login method 
-			if (GUI.Button(new Rect (552,240,120,40),"Login"))
+			if (GUI.Button(new Rect (252,240,120,40),"Login"))
 			{
 				message = "";
 				
@@ -94,11 +124,14 @@ public class Login_Register : MonoBehaviour {
 					//access the web page(in my case the login.php script) 
 					WWW w = new WWW("http://localhost:8080/login.php", form);
 					StartCoroutine(login(w));
+					//if(register){
+						//SaveSystem.LoadState();
+					//}
 				}
 			}
 			// if user click on the register button the boolen is set to true which goes to excute the first if statement
 			// on this OnGUI() method the result is a new ReGISteR FORM
-			if (GUI.Button(new Rect (702,240,120,40),"Register"))
+			if (GUI.Button(new Rect (382,240,120,40),"Register"))
 				register = true;	
 		}
 		//end of Login gui
@@ -106,36 +139,48 @@ public class Login_Register : MonoBehaviour {
 
 	//this method trys to login a user to the system it takes www(which is used to access web pages)
 	IEnumerator login(WWW w)
-	{
+	{//
 		yield return w;
-		if (w.error == null)
-		{
-			if (w.text == "login-SUCCESS")
-			{
-				print("WOOOOOOOOOOOOOOO!");
-			}
-			else
-				message += w.text;
-		}
-		else
-		{
-			message += "ERROR: " + w.error + "\n";
-		}
-	}
+			if (w.error == null)
+				{
+					if (w.text == "login-SUCCESS")
+						{
+							print("You have logged in !");
+							Application.LoadLevel(2);
+
+				            syst.LoadState();
+						}
+					else
+						message += w.text;
+				}
+			else{
+				  message += "ERROR: " + w.error + " ";
+				}
+	}//
 
 	//this method trys to register a user to the system it takes www(which is used to access web pages)
-	IEnumerator registerFunc(WWW w)
-	{
+	IEnumerator registerFunc(WWW w){
 		yield return w;
-		if (w.error == null)
-		{
-			message += w.text;
-		}
-		else
-		{
-			message += "ERROR: " + w.error + "\n";
-		}
+			if (w.error == null)
+		 	   {
+				message += w.text;
+			print("your registered and ready!");
+
+			     Application.LoadLevel(2);
+			syst.SaveState();
+			   }
+		else{
+			message += "ERROR: " + w.error + " ";
+			}
 	}
+	
+	
+//	public void saveScores() { 
+//		PlayerPrefs.SetString("pass",password);
+//	} 
+	
+	
+	//getScores() { score1 = PlayerPrefs.GetInt("pass"); } 
 }
 
 
